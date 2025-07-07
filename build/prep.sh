211010fcb2554231aff456e1cf142d859df16df4
Artem Lytvyn <letv1n2007@icloud.com>
2025-07-07 15:10:00 +0100

    editted built system script

--- build/prep.sh
+++ build/prep.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Building the program..."
+git clone git@github.com:letv1nnn/Web-Server.git && cd Wev-Server/
 cd server && cargo build --release && cd ..
 echo "Program has been built successfully."
