81159777365cd972c3e77b8f708ba949776d70df
Artem <122204149+letv1nnn@users.noreply.github.com>
2025-07-07 15:12:24 +0100

    editted build sys

--- build/prep.sh
+++ build/prep.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Building the program..."
-git clone git@github.com:letv1nnn/Web-Server.git && cd Wev-Server/
-cd server && cargo build --release && cd ..
+cd Wev-Server/server && cargo build --release && cd ..
 echo "Program has been built successfully."
